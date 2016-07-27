32d6f13c184aa25bfc93259cdb4d8aaaa6529513
chenhui <alex.qch@gmail.com>
2016-07-27 14:35:04 +0800

    get todo by id done

--- server.js
+++ server.js
@@ -25,7 +25,27 @@ app.get('/todos', function (req, res) {
 });
 
 // GET /todos/:id
+app.get('/todos/:id', (req, res)=>{
+    var todoId = req.params.id;
+    var matchedTodoObj;
+    //iterate over todo array and find a match
+    todos.forEach( (curVal)=>{
+        console.log(curVal.id);
+        if (curVal.id === todoId) {
+            console.log('matched todo obj found');
+            matchedTodoObj = curVal;
+        }
+    });
 
+    if (!matchedTodoObj) {
+        console.log('matched obj not found!');
+        //to send a 404: res.status(404).send();
+        res.status(404).send();
+    }else{
+        // send back json data
+        res.json(matchedTodoObj);
+    }
+});
 
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
