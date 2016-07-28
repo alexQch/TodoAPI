c93262697185893ce9b7ebdfc8a156e538f68dcc
Chenhui Quan <alex.qch@gmail.com>
2016-07-28 22:57:34 +0800

    play  with database

---
+++ playground/basic-sqlite-database.js
@@ -0,0 +1,58 @@
+var Sequelize = require('Sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+    'dialect': 'sqlite',
+    'storage': __dirname + '/basic-sqlite-database.sqlite'
+});
+
+var Todo = sequelize.define('todo', {
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1, 250]
+        }
+    },
+    completed: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+});
+
+sequelize.sync({force: false}).then( ()=>{
+    console.log('Everything is synced');
+
+    Todo.findById(3).then( (todo)=>{
+        if (todo) {
+            console.log(todo.toJSON());
+        }else{
+            console.log('todo not found');
+        }
+    });
+    /*Todo.create({
+        description: 'Walk my dog',
+        completed: false
+    }).then((todo)=>{
+        return Todo.create({
+            description: 'Clean trash office',
+        })
+    }).then( ()=>{
+        return Todo.findAll({
+            where: {
+                description: {
+                    $like: '%Office%'
+                }
+            }
+        })
+    }).then( (todos)=>{
+        if (todos) {
+            todos.forEach( (todo)=>{
+                console.log(todo.toJSON());
+            });
+        }else{
+            console.log('no TODO found');
+        }
+    }).catch((e)=>{
+        console.log
+    });*/
+});
