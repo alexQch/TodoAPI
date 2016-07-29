0e4f6f1cc44aa9f9a1e1e5d04a5ce94bf5d9cfe8
Chenhui Quan <alex.qch@gmail.com>
2016-07-29 19:50:24 +0800

    link db with server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require("underscore");
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -138,6 +139,9 @@ app.put('/todos/:id', (req, res)=>{
 
 });
 
-app.listen(PORT, function () {
-    console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then( ()=>{
+    app.listen(PORT, function () {
+        console.log('Express listening on port ' + PORT + '!');
+    });
 });
+
